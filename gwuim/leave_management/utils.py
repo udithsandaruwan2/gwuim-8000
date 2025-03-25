846e0f8851a684db4e041c8685105df777d424c5
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2025-03-25 12:17:58 +0530

    build leave management app views

---
+++ gwuim/leave_management/utils.py
@@ -0,0 +1,41 @@
+from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from employees.models import LeaveRequest
+
+def searchRequests(request):
+    search_query = request.GET.get('search', '')
+
+    # Use the double underscore syntax to filter by the 'name' field of the related models
+    requests = LeaveRequest.objects.distinct().filter(
+        Q(employee__full_name__icontains=search_query) |  # Corrected lookup for employee's name
+        Q(leave_type__name__icontains=search_query) |  # Corrected lookup for leave type's name
+        Q(request_type__icontains=search_query)  # Correct lookup for request_type
+    )
+    
+    return requests, search_query
+
+
+
+def paginateRequests(request, requests, results):
+    page = request.GET.get('page', 1)  # Default to page 1 if not provided
+    paginator = Paginator(requests, results)
+
+    try:
+        requests = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        requests = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        requests = paginator.page(page)
+
+    try:
+        page = int(page)
+    except ValueError:
+        page = 1  # Default to page 1 if conversion fails
+
+    left_index = max(page - 1, 1)
+    right_index = min(page + 2, paginator.num_pages + 1)
+
+    custom_range = range(left_index, right_index)
+    return custom_range, requests
--- gwuim/leave_management/views.py
+++ gwuim/leave_management/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .utils import searchRequests, paginateRequests
 
-# Create your views here.
+
+@login_required(login_url='login')
+def leaveRequets(request):
+    page = 'leave_requests'
+    page_title = 'Leave Requests'
+
+    # Queries With pagination
+    requests, search_query = searchRequests(request)
+    custom_range, requests = paginateRequests(request, requests, 10)
+
+    context = {
+        'page': page,
+        'page_title': page_title,
+        'requests': requests,
+        'search_query': search_query,
+        'custom_range': custom_range,
+        'requests':requests
+    }
+    return render(request, 'leave_management/leave_requests.html', context)
