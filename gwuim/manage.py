5151975038de67624b74dda7919fb797f2f21946
Udith Sandaruwan <developer.udithsandaruwan@gmail.com>
2025-04-03 09:49:15 +0530

    port changed

--- gwuim/manage.py
+++ gwuim/manage.py
@@ -2,7 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.commands.runserver import Command as runserver
 
+runserver.default_port = "8001"
 
 def main():
     """Run administrative tasks."""
@@ -15,10 +17,6 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    if len(sys.argv) == 1:
-        sys.argv.append("runserver")
-        sys.argv.append("8080")  # Change this to your desired port
-
     execute_from_command_line(sys.argv)
 
 
