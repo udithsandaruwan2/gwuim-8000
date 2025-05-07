21a14311820f8e01189f0223648c0681cfcf999a
sanu <info.dev.udith@gmail.com>
2025-05-07 08:40:36 +0530

    mew

--- gwuim/gwuim/settings.py
+++ gwuim/gwuim/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-mrt#6rvb+&8!i+zdqauq7a0+r=^2_*v0k&@%-3u6n@^f-s!e7^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- gwuim/users/forms.py
+++ gwuim/users/forms.py
@@ -1,9 +1,5 @@
-from django import forms
-from django.forms import ModelForm
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib.auth.models import User
-from .models import Profile
-
 
 class CustomUserCreationForm(UserCreationForm):
 
